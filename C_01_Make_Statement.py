d801acafbe9841e6ac89876ce8ed51a7221a43bf
DillionForger <gonzalesr24931@masseyhigh.school.nz>
2025-02-13 13:06:37 +1300

    Add files via upload

---
+++ C_01_Make_Statement.py
@@ -0,0 +1,10 @@
+
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decortion
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main Routine goes here
+make_statement("Hello everyone", "🌍")
